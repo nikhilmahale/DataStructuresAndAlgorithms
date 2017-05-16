f547f5f5bd739c10d8b9b59dc962edd066914670
Nikhil <Nikhil@LAPTOP-V8OC0PQT>
2017-05-16 20:32:26 +0530

    Draft copy for BST using singly link list.

---
+++ data_structures/nonLinear/bstUsingLinkList.cpp
@@ -0,0 +1,12 @@
+/**********************************************************
+ * Author: Nikhil Mahale
+ * Description: This code presents a program menu for a
+ * a binary search tree. It uses a doubly linked list to
+ * implement a Binary Search Tree.
+ * The program provides following functions:
+ * 1. Add Element
+ * 2. Search Element
+ * 3. Print Complete BST
+ * 4. Give height of BST
+ * 5. Exit Menu
+ **********************************************************/
